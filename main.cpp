539fd785b6df6087918844ac6b45a5767a3229e3
Leonid Kaida <leonid.kaida@uni-jena.de>
2020-01-29 15:36:53 +0100

    very simple and naive version

--- main.cpp
+++ main.cpp
@@ -1,60 +1,125 @@
-#include <mpi.h>
-
-#include <boost/mpi.hpp>
-
 #include <vector>
+#include <iostream>
 
-namespace mpi = boost::mpi;
+int main(int argc, char const *argv[]) {
+  int N, n_iter;
+  std::vector<bool> new_grid, old_grid;
+  int left, topleft, top, topright, right, bottomright, bottom, bottomleft;
+  int neighbours_sum;
+  int idx;
 
-int main() {
-  mpi::environment env;
-  mpi::communicator world;
-
-  std::cout << "I am process " << world.rank() << " out of " << world.size()
-            << " processes" << '\n';
-
-  if (world.rank() == 0 && world.size() > 1) {
-    world.send(1, 0, std::string("Hello"));
-    std::string msg;
-    world.recv(1, 0, msg);
-    std::cout << msg << "!" << std::endl;
-    world.send(1, 1, std::vector<double>{1, 2, 3});
-  } else if (1 == world.rank() && world.size() > 1) {
-    std::string msg;
-    world.recv(0, 0, msg);
-    std::cout << msg << ", ";
-    std::cout.flush();
-    world.send(0, 0, std::string("world"));
-    std::vector<double> v;
-    world.recv(0, 1, v);
-    for (auto e : v)
-      std::cout << "received: " << e << '\n';
+  if (argc > 1){
+    N = atoi(argv[1]);
+    n_iter = atoi(argv[2]);
+  } else {
+    N = 4;
+    n_iter = 1;
   }
 
-  std::string s = "ok boomer";
-  broadcast(world, s, 0);
-  std::cout << "rank " << world.rank() << " says: " << s << '\n';
-
-  if (0 == world.rank()) {
-    std::vector<int> numbers;
-    int mynumber = 42;
-    mpi::gather(world, mynumber, numbers, 0);
-    for (auto e : numbers)
-      std::cout << "rank 0 owns: " << e << '\n';
-  } else {
-    int mynumber = world.rank() * world.rank();
-    mpi::gather(world, mynumber, 0);
+  new_grid.reserve(N*N);
+  old_grid.reserve(N*N);
+  for (int i = 0; i < N*N; i++){
+    new_grid.push_back(0);
+    old_grid.push_back(0);
   }
 
-  auto magic = [](int a, int b) { return a + b; };
-  if (0 == world.rank()) {
-    int mynumber = 3432;
-    int minimum;
-    reduce(world, mynumber, minimum, magic, 0);
-    std::cout << "minimum is " << minimum << '\n';
-  } else {
-    int mynumber = world.rank() * world.rank();
-    reduce(world, mynumber, magic, 0);
+  old_grid[6] = 1;
+  old_grid[10] = 1;
+  old_grid[14] = 1;
+
+  for (int i = 0; i < N; i++){
+    for (int j = 0; j < N; j++)
+      std::cout << old_grid[i*N + j] << ' ';
+    std::cout << '\n';
+  }
+  std::cout << "------------" << '\n';
+
+  // game of life
+  for (int k = 0; k < n_iter; k++){
+    for (int i = 0; i < N; i++){
+      for (int j = 0; j < N; j++){
+        idx = i*N + j;
+        // neighbours:
+        // left
+        if (idx % N == 0){
+          left = idx - 1 + N;
+          // top left grid corner
+          if (idx < N)
+            topleft = N*N - 1;
+          else
+            topleft = idx - 1;
+          // left bottom corner of the grid
+          if (idx >= N*(N - 1))
+            bottomleft = N - 1;
+          // left corner of the grid
+          else
+            bottomleft = idx - 1 + N + N;
+        } else {
+          // if not left border
+          left = idx - 1;
+          if (idx < N)
+            topleft = N*(N - 1) + j - 1;
+          else
+            topleft = idx - 1 - N;
+          // if not left and bottom
+          if (idx >= N*(N - 1))
+            bottomleft = j - 1;
+          else
+            bottomleft = idx + N - 1;
+        }
+
+        if ((idx + 1)%N == 0){
+          right = i * N;
+          if (idx < N)
+            topright = N*(N - 1);
+          else
+            topright = idx - N + 1;
+          if (idx >= N*(N - 1))
+            bottomright = 0;
+          else
+            bottomright = idx + 1;
+        }
+        else{
+          right = idx + 1;
+          if (idx < N)
+            topright = N*(N - 1) + j + 1;
+          else
+            topright = idx - N + 1;
+          if (idx >= N*(N - 1))
+            bottomright = j + 1;
+          else
+            bottomright = idx + N + 1;
+        }
+
+        if (idx < N)
+          top = N*(N - 1) + j;
+        else
+          top = idx - N;
+
+        if (idx >= N*(N - 1))
+          bottom = j;
+        else
+          bottom = idx + N;
+
+        neighbours_sum = old_grid[left] + old_grid[right] + old_grid[top] + old_grid[bottom] +
+                     old_grid[topleft] + old_grid[topright] + old_grid[bottomleft] + old_grid[bottomright];
+
+        // std::cout << idx << ' ' << top << ' ' << left << ' ' << bottom << ' ' << right << '\n';
+        // std::cout << idx << ' ' << topleft << ' ' << topright << ' ' << bottomleft << ' ' << bottomright << '\n';
+
+        if (old_grid[idx] == 1)
+          new_grid[idx] = neighbours_sum >= 2 && neighbours_sum <= 3;
+        else
+          new_grid[idx] = neighbours_sum ==3;
+      }
+    }
+    new_grid.swap(old_grid);
+    for (int i = 0; i < N; i++){
+      for (int j = 0; j < N; j++)
+        std::cout << old_grid[i*N + j] << ' ';
+      std::cout << '\n';
+    }
+    std::cout << "--------" << '\n';
   }
 
   return 0;
